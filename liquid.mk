b951802dfb0593a5d39baafa0644a15f84023b28
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-04 11:39:20 -0400

    hltespr: common_phone
    Change-Id: Ifc9e2b701cd5c6411ad5271d9731fd9ec0c4370b

--- liquid.mk
+++ liquid.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/samsung/hltespr/full_hltespr.mk)
 $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_DEVICE := hltespr
 PRODUCT_NAME := liquid_hltespr
